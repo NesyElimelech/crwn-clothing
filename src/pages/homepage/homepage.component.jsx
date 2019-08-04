f52d3964be1d757024d6360552fa442b1015ef3d
NesyElimelech <nesy15@gmail.com>
2019-08-04 23:31:30 +0300

    Broken the homepage componenet into smaller component  and add styling to menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './hompage.styles.scss';
+import Directory from '../../components/directorty/directory.component'
+
+const HomePage = () => (
+    <div className = 'homepage'>
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
