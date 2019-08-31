7bff89c0cbb6259dba6cf1ccf83aa1274534b8f5
NesyElimelech <nesy15@gmail.com>
2019-08-31 13:07:21 +0300

    Add redux, and imploment userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils'
+import { connect } from 'react-redux';
 import './header.styles.scss'
 
 const Header = ({currentUser}) => (
@@ -28,4 +29,8 @@ const Header = ({currentUser}) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = stete => ({
+    currentUser: stete.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
-    <App/>
-</BrowserRouter>, document.getElementById('root'));
+        <App/>
+    </BrowserRouter>
+</Provider>, document.getElementById('root'));
