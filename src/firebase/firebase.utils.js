8ae70237af69b94da1abd4340a43ff8c8722d04e
NesyElimelech <nesy15@gmail.com>
2019-11-21 12:03:27 +0200

    bug fix

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,20 +12,18 @@ const config = {
   appId: "1:682194580841:web:cf925fcbdf19cd7d"
 };
 
-export const createUserProfileDocument = async (
-  userAuth,
-  ...additionalData
-) => {
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`Users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
@@ -34,20 +32,51 @@ export const createUserProfileDocument = async (
         ...additionalData
       });
     } catch (error) {
-      console.log("Error Creating a User", error.message);
+      console.log("error creating user", error.message);
     }
   }
+
   return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
+import React from "react";
+import { connect } from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 import {
   CollectionPageContainer,
   CollectionTitle,
   CollectionItemsContainer
-} from './collection.styles';
+} from "./collection.styles";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
